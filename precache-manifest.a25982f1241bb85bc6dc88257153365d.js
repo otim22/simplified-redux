d2337686984e918d39cd52b3f5b33537c7038682
Otim Fredrick <fredrickot@gmail.com>
2019-08-08 13:17:47 +0300

    Updates

---
+++ precache-manifest.a25982f1241bb85bc6dc88257153365d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b17a0aceceea9a6a223b9a20ed8d03b",
+    "url": "/simplified-redux/index.html"
+  },
+  {
+    "revision": "b50a9b873179fe16ba24",
+    "url": "/simplified-redux/static/js/2.3b3fc9de.chunk.js"
+  },
+  {
+    "revision": "97e61bfee3c0d6800556",
+    "url": "/simplified-redux/static/js/main.61ed8029.chunk.js"
+  },
+  {
+    "revision": "2548eb7b917f3c7ec9ab",
+    "url": "/simplified-redux/static/js/runtime~main.cf9bc14d.js"
+  }
+]);
